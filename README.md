443aac245724422909fdab6b2641e1fdad64b33f
Melissa Linkert <melissa@glencoesoftware.com>
2020-06-21 14:08:48 -0500

    Bypass SCIFIO readers and display scaled image

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-```bin/scaling-sandbox "test&pixelType=uint8&axes=X,Y&lengths=100000,100000.fake" data.tiff```
+Downsample by 50% and display resulting image:
+
+```bin/scaling-sandbox /path/to/test/file```
+
+Note that .fake files must be specified according to https://github.com/scifio/scifio/blob/37d1717f4aaa5b3c92f508bce69105efdc9e16d0/src/main/java/io/scif/io/location/TestImgLocation.java, e.g.:
+
+```
+test&pixelType=uint8&axes=X,Y&lengths=100000,100000.fake
+```
+
+Turning off the ```TestImgFormat``` does not bypass SCIFIO's fake file handling due to https://github.com/scifio/scifio/blob/37d1717f4aaa5b3c92f508bce69105efdc9e16d0/src/main/java/io/scif/io/location/TestImgLocationResolver.java
--- src/main/java/sandbox/Test.java
+++ src/main/java/sandbox/Test.java
@@ -1,29 +1,79 @@
 package sandbox;
 
+import io.scif.Format;
+import io.scif.SCIFIO;
+import io.scif.bf.BioFormatsFormat;
 import io.scif.img.ImgIOException;
 import io.scif.img.ImgOpener;
+import io.scif.img.ImgSaver;
+import io.scif.services.FormatService;
 
 import java.io.IOException;
 
-import net.imagej.ImageJ;
+import net.imglib2.FinalInterval;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
 import net.imglib2.img.cell.CellImgFactory;
+import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.interpolation.randomaccess.NLinearInterpolatorFactory;
+import net.imglib2.realtransform.RealViews;
+import net.imglib2.realtransform.Scale;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
+
+import org.scijava.io.location.FileLocation;
 
 public class Test {
     public static void main(String[] args) {
+        SCIFIO scifio = new SCIFIO();
         try {
-            ImgOpener opener = new ImgOpener();
-            final Img img = opener.openImg(args[0]);
+            // turn off all formats except Bio-Formats compatibility format
+            FormatService formats = scifio.format();
+            for (Format f : formats.getAllFormats()) {
+                f.setEnabled(f instanceof BioFormatsFormat);
+            }
 
-            ImageJ ij = new ImageJ();
-            double[] scaleFactors = new double[] {0.5, 0.5};
-            Object scaled = ij.op().run("scaleView", img, scaleFactors, new NLinearInterpolatorFactory());
-            ij.io().save(scaled, args[1]);
+            // open image from specified file
+            ImgOpener opener = new ImgOpener();
+            // no openImg(Location) signature
+            final Img img = opener.openImgs(new FileLocation(args[0])).get(0);
+
+            // downsample 50% in X and Y
+            // tried using the "scaleView" op, but matching is not reliable (maybe due to BioFormatsFormat?)
+
+						final long[] newDims = Intervals.dimensionsAsLongArray(img);
+            double[] scaleFactors = new double[newDims.length];
+            scaleFactors[0] = 0.5;
+            scaleFactors[1] = 0.5;
+						for (int i=0; i<newDims.length; i++) {
+                if (i >= 2) {
+                    scaleFactors[i] = 1.0 / newDims[i];
+                }
+						    newDims[i] = Math.round(newDims[i] * scaleFactors[i]);
+						}
+
+						NLinearInterpolatorFactory interpolator = new NLinearInterpolatorFactory();
+						IntervalView interval = Views.interval(Views.raster(RealViews.affineReal(
+								Views.interpolate(Views.extendMirrorSingle(img), interpolator),
+								new Scale(scaleFactors))), new FinalInterval(newDims));
+
+            // display image
+            ImageJFunctions.show(interval);
+
+            // save scaled image
+            // TODO: how to convert an IntervalView to an Img?
+            //ImgSaver saver = new ImgSaver();
+            //saver.saveImg(new FileLocation(args[1]), interval);
         }
-        catch (final ImgIOException|IOException e) {
+        catch (final Exception e) {
             e.printStackTrace();
+        }
+        finally {
+            // context disposal takes several seconds
+            /* debug */ System.out.println("shutting down...");
+            scifio.context().dispose();
         }
     }
 }
